20341d7c1d10749bf4c894badd2fe91c769801a8
nemeth.gergely <nemeth.gergely23@gmail.com>
2025-11-12 23:20:58 +0100

    - changed default redirect to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { TaskList } from './components/task-list/task-list';
 import { TaskForm } from './components/task-form/task-form';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'tasks', component: TaskList },
   { path: 'tasks/new', component: TaskForm },
-  { path: '**', redirectTo: '/tasks' }
+  { path: '**', redirectTo: '/login' }
 ];
